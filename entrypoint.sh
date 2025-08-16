073b70d8062f4a5d23cbc747675e7cc77b1fa61c
howhd7183 <ntun778.8@gmail.com>
2025-08-16 19:54:29 +0630

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,6 @@ useradd -m -s /bin/bash $SSH_USER
 echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 usermod -aG sudo $SSH_USER
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
-
-# SSHD config
 echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
-echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config.d/my_sshd.conf
-echo 'ChallengeResponseAuthentication no' >> /etc/ssh/sshd_config.d/my_sshd.conf
-
-# Ensure SSH host keys exist
-if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-    ssh-keygen -A
-fi
 
 exec "$@"
